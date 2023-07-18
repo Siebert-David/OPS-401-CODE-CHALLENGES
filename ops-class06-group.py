1321b288597a67da606276dca564ea93f8fc4302
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-07-18 13:22:32 -0400

    Create ops-class06-inclass.py

---
+++ ops-class06-group.py
@@ -0,0 +1,35 @@
+#1/usr/bin/python3
+
+# Script Name:                 401-ops-class06.py               
+# Class Name:                  Ops 401d8
+# Author Name:                 David Siebert 
+# Date of latest revision:     18JUL2023
+# In Python, In Python, create a script that utilizes the cryptography library to:
+
+#Prompt the user to select a mode:
+#Encrypt a file (mode 1)
+#Decrypt a file (mode 2)
+#Encrypt a message (mode 3)
+#Decrypt a message (mode 4)
+
+from cryptography.fernet import Fernet
+
+#declare variables
+y_n = "y"
+
+#define functions
+
+# function to load the key 
+def load_key():
+    return open
+
+#function to generate the key 
+def write_key():
+  key = Fernet.generate_key()
+  with open("key.key", "rb").read()
+
+
+
+
+  
+  
