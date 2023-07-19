8d0f82bddd96b9e3a4643fa2b678fdea52260da8
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-07-19 01:06:58 -0400

    Update 401-ops-class07.py

--- 401-ops-class07.py
+++ 401-ops-class07.py
@@ -4,7 +4,7 @@
 # Class Name:                  Ops 401d8
 # Author Name:                 David Siebert 
 # Date of latest revision:     19JUL2023
-# In Python create a script that utilizes OS.WALK to crawl the tree hierarchy.
+# In Python create a script that utilizes OS.WALK to crawl the tree hierarchy. Recursively encrypt a single folder and all its contents. Recursively decrypt a single folder that was encrypted by this tool.
 
 #root: Prints out directories only from what you specified
 #dirs: Prints out sub-directories from root
