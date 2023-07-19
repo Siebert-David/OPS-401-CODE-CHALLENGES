6050182b9197532c63e68ca87a79e05abd0dd044
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-07-18 23:44:16 -0400

    Update ops-class06a.py

--- ops-class06a.py
+++ ops-class06a.py
@@ -3,9 +3,8 @@
 # Script Name:                 401-ops-class06a.py               
 # Class Name:                  Ops 401d8
 # Author Name:                 David Siebert 
-# Date of latest revision:     18JUL2023
-# In Python, create a script that utilizes the cryptography library to encrypts a single file with a menu.
-
+# Date of latest revision:     19JUL2023
+# In Python, create a script that utilizes the cryptography library to encrypts a single file/message/data with a menu option.
 
 
 #import libraries
@@ -15,26 +14,34 @@ from os.path import exists
 # declare variables
 y_n = "y"
 
-# define functions
-
-# function to load the key 
-def load_key():
-    return open
+# define all functions
 
 # function to generate the key 
 def write_key():
   key = Fernet.generate_key()
-  with open("key.key", "rb").read()
+  with open("key.key", "wb") as key_file:
+      key_file.write(key)
+      
+# function to load the key 
+def load_key():
+    return open("key.key", "rb").read()
+    
+# function to encrypt a message
+def encrypt_message():
+    user_message = input("What message would you like to encrypt? ")
+    encrypted_message = user_message.encode()
+    f = Fernet(key)
 
-
-
+# function to encrypt the message 
+    encrypted = f.encrypt(encrypted_message)
+    print("your message encrypted: ")
+    print(encrypted)
+    
 # function to decypt the message
 def decrypt_message():
     user_message = input("What message would you like to decrypt? ")
     decrypted_message = str.encode(user_message)
 
-
-# function to encrypt a file
 
 # encrypt the data
 encrypted_file = f.encrypt(file_data)
