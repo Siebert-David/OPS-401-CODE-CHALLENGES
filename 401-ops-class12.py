3e7df1b49d58c165e841133209697df388d363cc
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-07-25 12:13:09 -0400

    Create 401-ops-class12.py

---
+++ 401-ops-class12.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Script Name:                 401-ops-class12.py               
+# Class Name:                  Ops 401d8
+# Author Name:                 David Siebert 
+# Date of latest revision:     26JUL2023
+# In Python, create a TCP Port Range Scanner that tests whether a TCP port is open or closed.
+
+#import Libraries
