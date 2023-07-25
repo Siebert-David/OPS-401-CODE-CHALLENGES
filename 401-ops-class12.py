929a601fea94eee284a0120aaf72135466f79d47
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-07-25 12:23:27 -0400

    Update 401-ops-class12.py

--- 401-ops-class12.py
+++ 401-ops-class12.py
@@ -7,3 +7,17 @@
 # In Python, create a TCP Port Range Scanner that tests whether a TCP port is open or closed.
 
 #import Libraries
+#import ipaddress # to provide subnet
+from ipaddress import IPv4Network
+from scapy.all import ICMP, IP, sr1, TCP
+
+
+#define variables 
+network="10.0.2.0/24"  #ask user for this action 
+ip_list = ipaddress.IPv4NEtework(network).hosts()
+print(ip_list)
+
+
+# Define Functions 
+
+#main
