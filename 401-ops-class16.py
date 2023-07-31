833b8ad4eb1b29a32ded43b3c288eb9e074e7fa2
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-07-31 12:36:13 -0400

    Update 401-ops-class16.py

--- 401-ops-class16.py
+++ 401-ops-class16.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+# Script Name:                 401-ops-class16.py               
+# Class Name:                  Ops 401d8
+# Author Name:                 David Siebert 
+# Date of latest revision:     01AUG2023
+#PArt 1 of 3 
+
 
 def iterator(): 
   # ask for filepath to the wordlist to utilize
@@ -8,7 +14,13 @@ def iterator():
   #open the wordlist file
   file = open(filepath, 'r')
 
+  #reads the first line of the wordlist and puts the variable called line
   line = file.readline()
+
+  
+  # while loop for password list
+  while line:
+  
   print(line)
   
   
