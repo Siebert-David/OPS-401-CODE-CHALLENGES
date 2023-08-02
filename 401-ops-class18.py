7fef7021ddf920f5e315aec330fc89d8cb851a87
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-08-02 13:16:27 -0400

    Create  401-ops-class18.py
    lab demo in class.

---
+++ 401-ops-class18.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Script Name:                 401-ops-class18.py               
+# Class Name:                  Ops 401d8
+# Author Name:                 David Siebert 
+# Date of latest revision:     02AUG2023
+# Automated Brute Force Wordlist Attack Tool Part 2 of 3. 
+
+# import libraries 
+from zipfile import ZipFile
+from getpass import getpass
+
+zip_file = input 
