ca1fa67175fb6f8d501da1f16c3e11022a49e103
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-08-14 12:09:56 -0400

    Create 401-ops-class26.py

---
+++ 401-ops-class26.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Script Name:                 401-ops-class26.py               
+# Class Name:                  Ops 401d8
+# Author Name:                 David Siebert 
+# Date of latest revision:     14AUG2023
+# Event logging tool Part 1 of 3. 
+
+
+# import libraries 
+import loggin 
+
+log = logging.getlogger(
