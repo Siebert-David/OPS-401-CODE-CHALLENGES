19d650142df419fb0996570cf3e6a897e90b341d
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-08-16 12:29:42 -0400

    2nd demo

--- ops-28.py
+++ ops-28.py
@@ -14,4 +14,29 @@ logger =logging.getLogger('my_logger')
 
 #creat handlers (stream and file ) 
 terminal_handler + logging.StreamHandler()
-file_handler = logging.FileHandler(filename)
+file_handler = logging.FileHandler('errors.log')
+
+#set levels for the handlers
+terminal_handler.setLevel(logging.WARNING)
+file_handler.setLevel(logging.ERROR)
+
+#Formatters for log messages
+terminal_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+#pair the formatters with handlers
+terminal_handler.setFormatter(terminal_format)
+file_handler.setFormatter(file_format)
+
+#add handlers to logger
+logger.addHandler(terminal_handler)
+logger.addHandler(file_handler)
+
+logger.warning("Warning Message")
+logger.error("Error Message")
+
+
+
+
+
