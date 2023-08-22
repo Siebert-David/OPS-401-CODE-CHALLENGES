ac11dbe3f6acd0a00d536ed62cfbb6c6fb8374b7
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-08-22 12:49:22 -0400

    3

--- ops-31a.py
+++ ops-31a.py
@@ -11,8 +11,11 @@
 #import libraries
 import os, time
 import platform 
+#from sys import platform 
 
 #define variables
+my_os = platform.system()
+print(my_os)
 
 #define functions 
 
@@ -23,9 +26,34 @@ def linuxSearch():
 
     #count and print the number of files searched
     os.system("ls " + str(directory) + " | echo \"searched $(wc -l) files.\"")
+    # $ grabs results \ is an escape character to ignore the "" in this case. also \*
+    $(cat demo_file.py)
+
+    #count and print the numbers of files found
+    os.system("find " + str(directory) + ' -name' + str(whichFile) + " -print \ echo \"Found $(grep -c/) files that match ")
+
+    print("")
+    os.system("find " + str(directory) + ' -name' + str(whichFile))
+    print("")
+
 
 #search for windows 
+def windowsSearch():
+    whichFile = input("what file are you looking for?")
+    directory = input("which directory would you like to search")
+
+    #count Number of files searched reading results of os.system(variable) so we can further process that data (variable)
+    searchCount = os.system("dir /a:-d /s /b " + str(directory) + " | find /c \":\\\"").read()
+
+    print("files searched " + searchCount)
+
+    # count number of files found and store as variable 
+    foundCount = os.system("dir /b/s " + str(directory) + str(whichFile) + " | find /c \":\\\"").read()
+                           
+      print("files found " + foundCount)
 
 
 
-#main
+    #main
+
+linuxSearch()
