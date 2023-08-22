074a8d9d5241add9ee9b476216940c180ca05a08
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-08-22 12:12:52 -0400

    review from marco

--- ops-31a.py
+++ ops-31a.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
 
+# Script Name:                 401-ops-class31.py               
+# Class Name:                  Ops 401d8
+# Author Name:                 David Siebert 
+# Date of latest revision:     21AUG2023
+# Part 2 of 3
+
+
+
 #import libraries
 import os, time
 import platform 
 
-#define variables. 
+#define variables
+
+#define functions 
+
+#search for linux
+def linuxSearch():
+    whichFile = input("what file are you looking for?")
+    directory = input("which directory would you like to search")
+
+    #count and print the number of files searched
+    os.system("ls " + str(directory) + " | echo \"searched $(wc -l) files.\"")
+    
