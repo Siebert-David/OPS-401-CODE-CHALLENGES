05d9e52ac1206e5d7a712f1e7b47af87010b1690
Siebert-David <130385666+Siebert-David@users.noreply.github.com>
2023-09-11 01:27:14 -0400

    Create ops_44.py

---
+++ ops_44.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+#import socket
+
+#sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#timeout = # TODO: Set a timeout value here.
+#sockmod.settimeout(timeout)
+
+#hostip = # TODO: Collect a host IP from the user.
+#portno = # TODO: Collect a port number from the user, then convert it to an integer data type.
+
+#def portScanner(portno):
+   # if sockmod.FUNCTION((hostip, portno)): # TODO: Replace "FUNCTION" with the appropriate socket.function call as found in the [socket docs](https://docs.python.org/3/library/socket.html)
+    #    print("Port closed")
+   # else:
+  #      print("Port open")
+
+#portScanner(port)
+
+#!/usr/bin/python3
+
+import socket
+
+sockmod = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+timeout = 5  # TODO: Set a timeout value here (e.g., 5 seconds).
+sockmod.settimeout(timeout)
+
+hostip = input("Enter the host IP: ")  # TODO: Collect a host IP from the user.
+portno = int(input("Enter the port number: "))  # TODO: Collect a port number from the user, then convert it to an integer data type.
+
+def portScanner(portno):
+    try:
+        sockmod.connect((hostip, portno))
+        sockmod.close()
+        print("Port open")
+    except ConnectionRefusedError:
+        print("Port closed")
+
+portScanner(portno)
